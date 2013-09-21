d2cf07f87c103e648ac2fd8396ceaf68a22198ec
Thiago Aguiar de Jesus <thiagosol@gmail.com>
2013-09-21 11:37:25 -0300

    move all code

---
+++ ambiente.bat
@@ -0,0 +1,9 @@
+set HOME=c:\users\Thiago\
+set PATH=%PATH%;c:\nuget\
+set CHROME_BIN="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+set IE_BIN=C:\Program Files\Internet Explorer\iexplore.exe
+set PHANTOMJS_BIN=%HOME%\AppData\Roaming\npm\phantomjs.cmd
+set FIREFOX_BIN=C:\Program Files (x86)\Mozilla Firefox\firefox.exe
+
+
+karma start
---
+++ karma.conf.js
@@ -0,0 +1,75 @@
+// Karma configuration
+// Generated on Wed Sep 04 2013 15:21:55 GMT-0300 (E. South America Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [      
+	  'Scripts/jquery-*.min.js',
+	  'Scripts/jquery.tmpl.min.js',
+	  'Scripts/jquery.tmpl.min.js',
+	  'Scripts/jquery.blockUI.min.js',
+	  'Scripts/bootstrap.min.js',      
+	  'Scripts/AsyncRequest.Queue.js',
+	  'Scripts/AsyncRequest.js',
+	  'spec/**/*Spec*.js'      
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
