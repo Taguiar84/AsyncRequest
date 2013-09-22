c801d09206da79c843198130fac1af36f493ea2c
Thiago Aguiar de Jesus <thiagosol@gmail.com>
2013-09-21 23:10:30 -0300

    corrigindo globalization pt-BR

--- Scripts/Globalization/AsyncRequest.pt-BR.js
+++ Scripts/Globalization/AsyncRequest.pt-BR.js
@@ -1,10 +1,12 @@
 ﻿; (function ($) {
-    $.asyncRequest.defaults = {
+    $.asyncRequest = $.asyncRequest || {};
+    $.asyncRequest.defaults =
+    $.extend(true, $.asyncRequest.defaults, {
         loadText: "Carregando..",
         notification: {
             notificationSuccessMsgDefault: 'Operação realizada com sucesso',
             notificationErrorMsgDefault: 'Ocorreu um erro ao realizar a operação',
             fullErroText: 'Erro Completo'
         }
-    }
+    });
 })(jQuery);
