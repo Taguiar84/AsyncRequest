f94997460cae837297bc0ab3d60346770f71e18c
Thiago Aguiar <thiago.sol@gmail.com>
2013-10-17 12:46:24 -0300

    melhorando teste #11 fix

--- spec/NotifyNotySpec.js
+++ spec/NotifyNotySpec.js
@@ -22,10 +22,11 @@ describe("Verifica notify - Noty", function () {
                 params.error('Sad Face');
             }, 5);
         });
+        $.noty.closeAll();
     });
 
     afterEach(function () {
-        $.noty.closeAll();        
+        $.noty.closeAll();
     });
 
     it("chamada basica", function () {
@@ -45,65 +46,142 @@ describe("Verifica notify - Noty", function () {
 
     });
 
-    //it("chamada com POST", function () {
-    //    runs(function () {
-    //        request.post(objRequest);
+    it("chamada com POST", function () {
+        runs(function () {
+            request.post(objRequest);
+        });
+        waits(1000);
+        runs(function () {
+            var valor = $(".noty_text:first").text();
+            //enconde html 
+            var expected = $('<div/>').text(request.defaults.notification.notificationSuccessMsgDefault).html();
+            expect(valor).toBe(expected);
 
-    //    });
-    //    waits(1000);
-    //    runs(function () {
-    //        var valor = $(".notificationTemplateMsg:first").text();
-    //        //enconde html 
-    //        var expected = $('<div/>').text(request.options.notification.notificationSuccessMsgDefault).html();
-    //        expect(valor).toBe(expected);
-            
-    //    });
-    //});
+        });
+    });
 
-    //it("chamada com GET, precisa habilitar isso", function () {
-    //    runs(function () {
-    //        request.init({ notification: { notifyCommandType: ["GET"] } });
-    //        request.get(objRequest);
+    it("chamada com GET, precisa habilitar isso", function () {
+        runs(function () {
+            request.get(objRequest, { notification: { notifyCommandType: ["GET"] } });
 
-    //    });
-    //    waits(1000);
-    //    runs(function () {
-    //        var valor = $(".notificationTemplateMsg:first").text();
-    //        expect(valor).toBe(request.options.notification.notificationSuccessMsgDefault);
-    //    });
-    //});
+        });
+        waits(1000);
+        runs(function () {
+            var valor = $(".noty_text:first").text();
+            expect(valor).toBe(request.defaults.notification.notificationSuccessMsgDefault);
+        });
+    });
 
-    //it("personalizando MSG de sucesso e erro", function () {
-    //    var msg = "Custom Mensage";
-    //    var msgErro = msg + " erro";
-    //    runs(function () {
-    //        request.init({ notification: { notificationSuccessMsgDefault: msg, notificationErrorMsgDefault: msgErro } });
-    //        request.post(objRequest);
-
-    //    });
-    //    waits(1000);
-    //    runs(function () {
-    //        var valor = $(".notificationTemplateMsg:first").text();
-    //        expect(valor).toBe(msg);
-
-    //        var valor = $(".notificationTemplateMsg:last").text();
-    //        expect(valor).toBe(msgErro);
-    //    });
-    //});
+    it("desabilita o notify", function () {
+        runs(function () {
+            request.post(objRequest, { notification: { notifyFunction: false } });
+        });
+        waits(1000);
+        runs(function () {
+            var valor = $(".noty_text").length;
+            expect(valor).toBe(0);
+        });
+    });
 
 
-    //it("personalizando Notification", function () {
+    it("personalizando MSG de sucesso e erro", function () {
+        var msg = "Custom Mensage";
+        var msgErro = msg + " erro";
+        runs(function () {
+            request.post(objRequest, { notification: { notificationSuccessMsgDefault: msg, notificationErrorMsgDefault: msgErro } });
 
-    //    var func = jasmine.createSpy("notify");
-    //    runs(function () {
-    //        request.init({ notification: { notifyFunction: func, notificationSuccessMsgDefault: "Funcção Personalizada", notificationErrorMsgDefault: "Funcção Personalizada erro" } });
-    //        request.post(objRequest);
-    //    });
-    //    waits(1000);
-    //    runs(function () {
-    //        expect(func).toHaveBeenCalled();
-    //    });
-    //});
+        });
+        waits(1000);
+        runs(function () {
+            var valor = $(".noty_text:first").text();
+            expect(valor).toBe(msg);
+
+            var valor = $(".noty_text:last").text();
+            expect(valor).toBe(msgErro);
+        });
+    });
 
 
+    it("personalizando Notification", function () {
+
+        var func = jasmine.createSpy("notify");
+        runs(function () {
+            request.post(objRequest, { notification: { notifyFunction: func, notificationSuccessMsgDefault: "Funcção Personalizada", notificationErrorMsgDefault: "Funcção Personalizada erro" } });
+        });
+        waits(1000);
+        runs(function () {
+            expect(func).toHaveBeenCalled();
+        });
+    });
 });
+
+
+describe("Verifica notify Erro - Noty", function () {
+    var request;
+    var objRequest;
+    beforeEach(function () {
+        //Disable Jgrowl
+        $.jGrowl = null;
+        request = $.asyncRequest;
+        objRequest = request.getObjAsync();
+        spyOn($, "ajax").andCallFake(function (params) {
+            setTimeout(function () {
+                params.success('Happy Face');
+                params.error({ responseText: 'Sad Face' });
+            }, 5);
+        });
+        $.noty.closeAll();
+    });
+
+    afterEach(function () {
+        $.noty.closeAll();
+
+    });
+
+    it('Request com erro, deve ter leitura de response text como erro', function () {
+
+        runs(function () {
+            request.post(objRequest);
+        });
+
+        waits(1000);
+        runs(function () {
+            var valor = $(".noty_text:last").text();
+            expect(valor).toBe('Sad Face');
+        });
+    });
+});
+//DON'T HAVE
+//describe("Verifica notify Erro JSON - Noty", function () {
+//    var request;
+//    var objRequest;
+//    beforeEach(function () {
+//        //Disable Jgrowl
+//        $.jGrowl = null;
+//        request = $.asyncRequest;
+//        objRequest = request.getObjAsync();
+//        spyOn($, "ajax").andCallFake(function (params) {
+//            setTimeout(function () {
+//                params.success('Happy Face');
+//                params.error({ responseText: "{\"errorMessage\":\"another\",\"StackTrace\":\"stack trace\", \"ExceptionMessage\": \"Exception Message\"}" });
+//            }, 5);
+//        });
+//        $.noty.closeAll();
+//    });
+
+//    afterEach(function () {
+//        $.noty.closeAll();
+//    });
+
+//    it('Request com erro, deve ter leitura de response text como erro', function () {
+
+//        runs(function () {
+//            request.post(objRequest);
+//        });
+//        waits(1000);
+//        runs(function () {
+//            valor = $(".msgStack li:last").text(); //Stack
+//            expect(valor).toBe('Exception Messagestack trace');
+//        });
+//    });
+//});
