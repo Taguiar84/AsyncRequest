5a444ebe3e2a6d1ce270cb40a5f96dbfe836b59e
Taguiar84 <thiago.sol@gmail.com>
2013-11-22 14:17:58 -0300

    colocando Globalization

---
+++ javascripts/Globalization/AsyncRequest.pt-BR.js
@@ -0,0 +1,12 @@
+﻿; (function ($) {
+    $.asyncRequest = $.asyncRequest || {};
+    $.asyncRequest.defaults =
+    $.extend(true, $.asyncRequest.defaults, {
+        loadText: "Carregando..",
+        notification: {
+            notificationSuccessMsgDefault: 'Operação realizada com sucesso',
+            notificationErrorMsgDefault: 'Ocorreu um erro ao realizar a operação',
+            fullErroText: 'Erro Completo'
+        }
+    });
+})(jQuery);
